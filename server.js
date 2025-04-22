48237978064e47f3730465300f10ee32adf7fd85
kushwahachandanbtry <kushwahachandan44@gmail.com>
2025-04-22 09:10:01 +0545

    Removing all files.

--- server.js
+++
@@ -1,7 +0,0 @@
-const http = require('http');
-const port = 3000;
-const app = require('../Blog API/app');
-
-const server = http.createServer(app);
-
-server.listen(port, () => {console.log('App is running on port: ', port)})
